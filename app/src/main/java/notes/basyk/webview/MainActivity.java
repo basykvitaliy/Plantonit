72aac589e837b7dedb83ca0dd832c6d53ca752bb
basykvitaliy <60960150+basykvitaliy@users.noreply.github.com>
2020-07-06 08:39:31 +0300

    Add url

---
+++ app/src/main/java/notes/basyk/webview/MainActivity.java
@@ -0,0 +1,39 @@
+package notes.basyk.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    private static final String BASE_URL = "https://plantonit.ua/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.myWebView);
+        webView.loadUrl(BASE_URL);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+
+        }
+        super.onBackPressed();
+    }
+}
