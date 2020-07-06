9793e2fda80689e306dd0ff6030247a2b1542edf
basykvitaliy <60960150+basykvitaliy@users.noreply.github.com>
2020-07-06 11:48:56 +0300

    Add connect

--- app/src/main/java/notes/basyk/webview/MainActivity.java
+++ app/src/main/java/notes/basyk/webview/MainActivity.java
@@ -1,21 +1,34 @@
 package notes.basyk.webview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
 
     private static final String BASE_URL = "https://plantonit.ua/";
     private ProgressBar progressBarWeb;
+    private ProgressDialog progressDialog;
+    private RelativeLayout relativeLayout;
+    private Button buttonNoInternet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +38,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         progressBarWeb = findViewById(R.id.progressBar);
+//        progressDialog = new ProgressDialog(this);
+//        progressDialog.setMessage("Загрузка. Пожалуйста подождите.");
+
+        buttonNoInternet = findViewById(R.id.bottomNoCon);
+        relativeLayout = findViewById(R.id.relativeLayout);
+
         webView = findViewById(R.id.myWebView);
-        webView.loadUrl(BASE_URL);
+        checkConnection();
+
         webView.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -41,11 +61,22 @@ public class MainActivity extends AppCompatActivity {
 
             progressBarWeb.setVisibility(View.VISIBLE);
             progressBarWeb.setProgress(newProgress);
+            setTitle("Загрузка...");
+            //progressDialog.show();
             if (newProgress == 100){
                 progressBarWeb.setVisibility(View.GONE);
+                setTitle(view.getTitle());
+                //progressDialog.dismiss();
             }
 
                 super.onProgressChanged(view, newProgress);
+            }
+        });
+
+        buttonNoInternet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkConnection();
             }
         });
     }
@@ -57,6 +88,52 @@ public class MainActivity extends AppCompatActivity {
         }else {
             super.onBackPressed();
         }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.back:
+                onBackPressed();
+                break;
+                case R.id.go:
+                    if (webView.canGoForward()){
+                        webView.goForward();
+                    }
+                    break;
+            case R.id.reload:
+                checkConnection();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void checkConnection() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobileNetwork = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        if (wifi.isConnected()){
+            webView.loadUrl(BASE_URL);
+            webView.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility(View.GONE);
+
+        }else if (mobileNetwork.isConnected()){
+            webView.loadUrl(BASE_URL);
+            webView.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility(View.GONE);
+        }else {
+            webView.setVisibility(View.GONE);
+            relativeLayout.setVisibility(View.VISIBLE);
+        }
     }
 }
