ee41d0aa2eb37a7a72ccbbf175bd4382ea7634cb
basykvitaliy <60960150+basykvitaliy@users.noreply.github.com>
2020-07-06 10:15:04 +0300

    Add progressBar

--- app/src/main/java/notes/basyk/webview/MainActivity.java
+++ app/src/main/java/notes/basyk/webview/MainActivity.java
@@ -3,19 +3,28 @@ package notes.basyk.webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
 
     private static final String BASE_URL = "https://plantonit.ua/";
+    private ProgressBar progressBarWeb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        progressBarWeb = findViewById(R.id.progressBar);
         webView = findViewById(R.id.myWebView);
         webView.loadUrl(BASE_URL);
         webView.setWebViewClient(new WebViewClient(){
@@ -25,6 +34,20 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+
+            progressBarWeb.setVisibility(View.VISIBLE);
+            progressBarWeb.setProgress(newProgress);
+            if (newProgress == 100){
+                progressBarWeb.setVisibility(View.GONE);
+            }
+
+                super.onProgressChanged(view, newProgress);
+            }
+        });
     }
 
     @Override
@@ -32,8 +55,8 @@ public class MainActivity extends AppCompatActivity {
         if (webView.canGoBack()){
             webView.goBack();
         }else {
-
+            super.onBackPressed();
         }
-        super.onBackPressed();
+
     }
 }
